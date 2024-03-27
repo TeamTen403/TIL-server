64123736bb877295b5d25bc1374a8e7e868f155b
Songwonseok <dnjstjr0517@gmail.com>
2024-03-27 16:40:31 +0900

    feat: 메시지 수정

--- src/main/java/com/teamten/til/common/dto/ResponseType.java
+++ src/main/java/com/teamten/til/common/dto/ResponseType.java
@@ -8,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum ResponseType {
 	SUCCESS("SUCCESS", "성공"),
 	ERROR("ERROR", "서버에 오류가 발생하였습니다."),
-	NOT_LOGIN("NOT_LOGIN", "로그인이 필요한 이벤트입니다."),
-	ALREADY("ALREADY", "이미 참여하였습니다."),
+	NOT_LOGIN("NOT_LOGIN", "로그인이 필요합니다."),
+	ALREADY("ALREADY", "이미 생성했습니다."),
 	INVALID("INVALID", "유효하지 않은 요청입니다.");
 
 	private final String code;
