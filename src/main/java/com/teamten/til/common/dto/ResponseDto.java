66c6cc0fc233a806aeadbf1b8bd32b70b48b4d7e
Songwonseok <dnjstjr0517@gmail.com>
2024-04-02 19:43:58 +0900

    docs: @Schema example 반영

--- src/main/java/com/teamten/til/common/dto/ResponseDto.java
+++ src/main/java/com/teamten/til/common/dto/ResponseDto.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,8 +13,10 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class ResponseDto<T> implements Serializable {
+	@Schema(description = "응답코드", example = "SUCCESS")
 	private String code;
 	@JsonInclude(JsonInclude.Include.NON_NULL)
+	@Schema(description = "데이터", nullable = true)
 	private T data;
 
 	public static <T> ResponseDto<T> code(ResponseType responseType) {
